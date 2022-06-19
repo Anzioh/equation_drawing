fda8d19f4a54385dda007c7cff2fa7b28d7bab1c
Chenghsuan Ho <chenghsuan@protonmail.com>
2022-06-20 02:33:31 +0800

    chore: remove vue-router and vuex

--- GUI/src/main.ts
+++ GUI/src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-// import router from './router'
-// import store from './store'
+import App from "./App.vue";
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
@@ -15,7 +13,5 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
 }
 app.use(pinia);
-// app.use(store);
-// app.use(router);
 app.use(ElementPlus);
 app.mount('#app');
