e88451d7f972e8a4ac11ce428bd04c8d14e99f17
王正宏 <wangzhenghong@wangzhenghongs-iMac.local>
2022-06-24 13:51:18 +0800

    fix es5-ext plugin path problem

---
+++ GUI/plugin/es5-ext/object/get-property-names.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var uniq                = require("../arraymainuniq")
+  , value               = require("./valid-value")
+  , push                = Array.prototype.push
+  , getOwnPropertyNames = Object.getOwnPropertyNames
+  , getPrototypeOf      = Object.getPrototypeOf;
+
+module.exports = function (obj) {
+	var keys;
+	obj = Object(value(obj));
+	keys = getOwnPropertyNames(obj);
+	while ((obj = getPrototypeOf(obj))) {
+		push.apply(keys, getOwnPropertyNames(obj));
+	}
+	return uniq.call(keys);
+};
